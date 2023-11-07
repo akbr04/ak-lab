bed20774f9a26d610cd83c9c0055a1022bcbc80f
ak2130 <145095305+akbr04@users.noreply.github.com>
2023-11-07 11:53:28 +0400

    sm

---
+++ week 4/weather app/app.js
@@ -0,0 +1,39 @@
+const apiKey = 'cfd2128ebb9f5b38b56d4a612a0f55c9';
+const cityInput = document.getElementById('cityInput');
+const getWeatherButton = document.getElementById('btn');
+const weatherInfo = document.getElementById('weather-info');
+
+getWeatherButton.addEventListener('click', () => {
+  const city = cityInput.value;
+
+  if (city === '') {
+    alert('Please enter a city name.');
+    return;
+  }
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const weatherDescription = data.weather[0].description;
+      const temperature = data.main.temp;
+      const windSpeed = data.wind.speed;
+
+      const weatherMessage = `Weather: ${weatherDescription}<br>Temperature: ${temperature}°C<br>Wind Speed: ${windSpeed} m/s`;
+
+      weatherInfo.innerHTML = weatherMessage;
+    })
+    .catch((error) => {
+      if (error instanceof Error) {
+        alert('An error occurred: ' + error.message);
+      } else {
+        alert('An error occurred. Please try again later.');
+      }
+    });
+});
