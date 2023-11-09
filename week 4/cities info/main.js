48ffc9ac1c9216a33448e988a77d52e13b0219c6
ak2130 <145095305+akbr04@users.noreply.github.com>
2023-11-09 14:39:50 +0400

    k

--- week 4/cities info/main.js
+++ week 4/cities info/main.js
@@ -1,21 +1,21 @@
-
-var cityContainer = document.getElementById("city-info");
+var cityContainer = document.getElementById("city-info");   
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://akbr04.github.io/ak-lab/week%204/cities1.json');
-ourRequest.onload = function() {
-var ourData = JSON.parse(ourRequest.responseText);
-renderHTML(ourData);
-btn.classList.add("hide-me");
-};
-ourRequest.send();
-});
+
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://akbr04.github.io/ak-lab/week%204/cities1.json');
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+})
 
 function renderHTML(data){
     var htmlString = "";
     for (i=0; i<data.length; i++){
-    htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br> Where you can enjoy indoor places like: " ;
+    htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + 
+    ",</br> Where you can enjoy indoor places like: " ;
     for (ii = 0; ii < data[i].places.indoor.length; ii++) {
     // Loop through the indoor places of the current city.
     if (ii == 0) {
@@ -34,7 +34,7 @@ function renderHTML(data){
     }
     }
     htmlString += '.</p>';
+    
     }
     cityContainer.insertAdjacentHTML('beforeend' , htmlString);
     }
-    
