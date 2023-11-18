a983f9b9a8bc601c5c0e43f70beed46e5fdb075c
ak2130 <145095305+akbr04@users.noreply.github.com>
2023-11-18 21:28:09 +0400

    ok

--- week 5/index.js
+++ week 5/index.js
@@ -1,10 +1,39 @@
-const express = require("express")
+const express = require("express");
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: './info.env'});
 
 const app = express();
 
-app.get("/", (req, res) => 
-{ res.send("<h1>Home Page</h1>") });
+const data_b = mysql.createConnection({
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.info   
+});
 
-app.listen(6000, () => 
-{ console.log("Server started on Port 6000"); })
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
 
+app.set('view engine', 'hbs');
+
+data_b.connect( (error) => {
+      if(error) {
+        console.log(error)
+      } else {
+        console.log("MYSQL Connected...")
+      }
+})
+
+app.get("/", (req, res) => {
+  res.render("PROFILE"); 
+})
+
+app.listen(5000, () => {
+    console.log("Server started on Port 5000");
+})  
+
+
+  
