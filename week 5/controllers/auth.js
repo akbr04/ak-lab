da4574180ee9df5a2c1c75cad625444b46d88879
ak2130 <145095305+akbr04@users.noreply.github.com>
2023-11-22 11:48:19 +0400

    k

--- week 5/controllers/auth.js
+++ week 5/controllers/auth.js
@@ -1,4 +1,28 @@
+const express = require('express');
+const mysql = require("mysql");
+const data_b = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.info   
+  });
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form Submitted");
-};
+
+    const {name, email, password, confirm_password} = req.body
+    data_b.query('SELECT email FROM users WHERE email = ?', [email],(error, result) =>{
+        if (error) {
+            console.log(error);
+        }
+        if (result.length > 0) {
+            return res.render('register`') {
+                message: 'This email is already taken.....'
+            }
+        } else if (password !== confirm_password){
+            return res.render('register', {
+                message: 'Passwords do not match'
+            });
+        }
+    });
+}
